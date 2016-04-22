045422782e99b933f04ec52553fbd52626da4d64
Adam Gandelman <adamg@ubuntu.com>
2016-04-21 19:20:03 -0700

    Remove MOS pins on the astara node
    This removes MOS apt pins to ensure we pull all the mitaka
    python requirements down with the astara packages.
    
    It does leave OVS pinned so we do not deviate from the installed
    versions on compute/etc.
    
    Also fixes up spacing in repo.pp

--- deployment_scripts/puppet/modules/astara/manifests/repo.pp
+++ deployment_scripts/puppet/modules/astara/manifests/repo.pp
@@ -1,31 +1,59 @@
 class astara::repo {
-   include apt
-   $role = hiera('role')
-   if hiera('fuel_version') == '8.0' {
-      if $role == 'controller' or $role == 'primary-controller' {
-              notice('MODULAR: astara - Installing controller version for Liberty')
-              apt::ppa { 'ppa:astara-drivers/astara-liberty': }
-              exec {
-                'apt-get update':
-                path => '/usr/bin/',
-                require => Apt::Ppa['ppa:astara-drivers/astara-liberty'] }
-      } else {
-              notice('MODULAR: astara - Installing Mitaka version for Liberty')
-              package { 'ubuntu-cloud-keyring': ensure => 'present' }
-              apt::ppa { 'ppa:astara-drivers/astara-mitaka': }
-              apt::source { 'ubuntu-cloud':
-                  location          =>  'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-                  repos             =>  'main',
-                  release           =>  'trusty-updates/mitaka',
-                  include           => { 'src' => false },
-                  require           => Package['ubuntu-cloud-keyring'],
-              }
-              exec {
-                'apt-get update':
-                path => '/usr/bin/',
-                require => [Apt::Ppa['ppa:astara-drivers/astara-mitaka'], Apt::Source['ubuntu-cloud']] }
+    include apt
+    $role = hiera('role')
+    if hiera('fuel_version') == '8.0' {
 
-       }
+        if $role == 'controller' or $role == 'primary-controller' {
+            # we install liberty on all nodes except the astara nodes
+            notice('MODULAR: astara - Installing controller version for Liberty')
+            apt::ppa { 'ppa:astara-drivers/astara-liberty': }
+            exec {
+                'apt-get update':
+                path => '/usr/bin/',
+                require => Apt::Ppa['ppa:astara-drivers/astara-liberty']
+            }
+
+        } else {
+            notice('MODULAR: astara - Installing Mitaka version for Liberty')
+            package { 'ubuntu-cloud-keyring': ensure => 'present' }
+            apt::ppa { 'ppa:astara-drivers/astara-mitaka': }
+            apt::source { 'ubuntu-cloud':
+                location =>  'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+                repos => 'main',
+                release =>  'trusty-updates/mitaka',
+                include => { 'src' => false },
+                require => Package['ubuntu-cloud-keyring'],
+            }
+
+            # when we are installing mitaka on astara node alongside liberty elsewhere,
+            # we need to ensure we pull down the correct dependencies, so we must remove
+            # the MOS pins to allow mitaka requirements.  we omit OVS from this to keep
+            # consitency there.
+            exec { 'remove-mos-pins':
+                command => '/bin/rm /etc/apt/preferences.d/*.pref',
+            }
+
+            file { 'ovs-pin':
+                ensure  => present,
+                path    => '/etc/apt/preferences.d/ovs.pref',
+                source  => 'puppet:///modules/astara/ovs.pref',
+                replace => true,
+                owner   => '0',
+                group   => '0',
+                mode    => '0644',
+                require => Exec['remove-mos-pins'],
+            }
+
+            exec {
+                'apt-get update':
+                path => '/usr/bin/',
+                require => [
+                    Apt::Ppa['ppa:astara-drivers/astara-mitaka'],
+                    Apt::Source['ubuntu-cloud'],
+                    File['ovs-pin']
+                ]
+            }
+        }
    } else {
       fail('Currently Astara deployment supported only with Fuel 8.0/liberty')
    }
